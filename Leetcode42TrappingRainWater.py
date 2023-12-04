2ea328ad0d04db38c9b011e56f12898276397ce8
Brian5190 <151711273+Brian5190@users.noreply.github.com>
2023-12-04 14:10:47 +0800

    no message

--- Leetcode42TrappingRainWater.py
+++ Leetcode42TrappingRainWater.py
@@ -1,43 +1,40 @@
-height = [0,1,0,2,1,0,1,3,2,1,2,1]
-count = 0
-left = 0
-right = 0
-temp = []
-result = 0
-while count < len(height):
-    if height[left] > height[count]:
-        right = count
-        temp.append(count)
-        count += 1
-
-        
-    else:
-        if len(temp) > 1:
-            temp.append(count)
-            right = count
-
-        for i in range(len(temp)):
-            #print(height[left], height[right], height[temp[i]])
-            temp[i] = max(0, min(height[left], height[right]) - height[temp[i]])
-
-        result += sum(temp)
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        count = 0
+        left = 0
+        right = 0
         temp = []
-        left = count
-        right = count
-        count += 1
-print(temp)
-print(left,right)
-'''
-if left != right:
-    while(right != left and height[right] < height[right - 1]):
-        right -= 1
-    #print(right, left)
-    temp = temp[0 : right - left + 1]
-    for i in range(0, right - left + 1):
-        print(height[left], height[right], height[temp[i]])
-        print(height[left], height[right], height[temp[i]])
-        temp[i] = max(0, min(height[left], height[right]) - height[temp[i]])    
-    print(temp)
-    result += sum(temp[:-1])
-'''
-print(result)
+        result = 0
+        for i in range(len(height)):
+            right = i
+            temp.append(height[i])
+            
+            if height[right] >= height[left]:
+
+                if height[left] > min(temp) and height[right] > min(temp):
+                    result += min(height[right], height[left]) * len(temp) - sum(temp) + abs(height[right] - height[left])
+
+                temp = []
+                left = right
+                temp.append(height[i])
+
+        right = len(temp) - 1
+        left = len(temp) - 1
+
+        if len(temp) > 2:
+            temp2 = []
+
+            for i in range(len(temp) - 1, -1, -1):
+                left = i
+                temp2.append(temp[i])
+
+                if temp[right] <= temp[left]:
+
+                    if temp[right] > min(temp2) and temp[left] > min(temp2):
+                        result += min(temp[right], temp[left]) * len(temp2) - sum(temp2) + abs(temp[right] - temp[left])
+                        
+                    right = left
+                    temp2 = []
+                    temp2.append(temp[i])
+        return result
+                    
