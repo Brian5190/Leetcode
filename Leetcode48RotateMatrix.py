e0a82693090d3b5e70980ecc95e4fbd5ff6cbba4
Brian5190 <151711273+Brian5190@users.noreply.github.com>
2023-12-06 12:08:38 +0800

    no message

--- Leetcode48RotateMatrix.py
+++ Leetcode48RotateMatrix.py
@@ -1,7 +1,18 @@
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
-a = [1,2,3]
-b = [4,5,6]
-c = [4,5,6,7,8]
-d = zip()
-zipped = d.(a,b)
-print(zipped)
+
+for i in range(len(matrix)):
+	for j in range(i):
+		matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+print(matrix)
+
+
+for i in range(len(matrix)):
+    left, right = 0, len(matrix) - 1
+	
+    while left < right : 
+        matrix[i][left], matrix[i][right] = matrix[i][right], matrix[i][left]
+        left += 1
+        right -= 1
+
+print(matrix)
